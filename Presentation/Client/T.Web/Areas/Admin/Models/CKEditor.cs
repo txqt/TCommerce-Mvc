44533e794582f40257238eb359095d078d297a5f
T <hovanthanh12102002@gmail.com>
2023-11-13 14:12:07 +0700

    backup

---
+++ Presentation/Client/T.Web/Areas/Admin/Models/CKEditor.cs
@@ -0,0 +1,21 @@
+﻿namespace T.Web.Areas.Admin.Models
+{
+    public class CKEditor
+    {
+        public CKEditor(string idEditor, bool loadLibrary = true)
+        {
+            IdEditor = idEditor;
+            LoadLibrary = loadLibrary;
+        }
+        public string IdEditor { get; set; }
+        public bool LoadLibrary { get; set; }
+        public int height { get; set; } = 200;
+        public string toolbar { get; set; } = @"
+    [
+        { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+        // Add more toolbar items as needed
+    ]
+";
+    }
+}
